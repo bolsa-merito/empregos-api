a75ef1400fd15a364f3b66af737ac93d03f4d0a3
Hugo Batista <hugobatista2710@gmail.com>
2025-05-31 22:55:44 -0300

    feat: cria o model da entidade Student

---
+++ app/Models/Student.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'birth_date',
+        'looking_for_internship',
+        'description',
+        'contact_email',
+        'phone_number',
+        'user_id',
+    ];
+
+    protected $casts = [
+        'birth_date' => 'date',
+        'looking_for_internship' => 'boolean',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
+
