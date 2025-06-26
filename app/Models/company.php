dcbae2525290450e9c1bd97393b28eaceb9932b3
THOMASW0LF <thomasgwolf1@gmail.com>
2025-06-26 17:05:28 -0300

    corrige model e migration de company

--- app/Models/company.php
+++ app/Models/company.php
@@ -4,7 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class company extends Model
+class Company extends Model
 {
-    //
+    use HasFactory;
+    
+    public function address()
+    {
+        return $this->belongsTo(Address::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function benefits()
+    {
+        return $this->hasMany(Benefit::class);
+    }
+
+    public function connections()
+    {
+        return $this->hasMany(Connection::class);
+    }
+
+    public function favoriteStudents()
+    {
+        return $this->hasMany(CompanyFavoriteStudent::class);
+    }
+
 }
