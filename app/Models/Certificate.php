48cec40552bcbf9fd1966b5130c8c60dc4b2b5b5
THOMASW0LF <thomasgwolf1@gmail.com>
2025-07-01 16:49:09 -0300

    Atualiza model de Certificate com $fillable e $casts

--- app/Models/Certificate.php
+++ app/Models/Certificate.php
@@ -3,10 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Certificate extends Model
 {
     use HasFactory;
+
+     /**
+     * Campos preenchíveis em massa
+     */
+    protected $fillable = [
+        'student_id',
+        'institution',
+        'course_name',
+        'course_load',
+    ];
+
+    /**
+     * Casts automáticos de datas
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
     
     public function student()
     {
