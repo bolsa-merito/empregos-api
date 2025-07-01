46eea63acdeafd3d7d3225fcd5ec4924a2dfbd3a
THOMASW0LF <thomasgwolf1@gmail.com>
2025-07-01 16:48:37 -0300

    Atualiza model de BenefitTemplate com $fillable e $casts

--- app/Models/BenefitTemplate.php
+++ app/Models/BenefitTemplate.php
@@ -3,8 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class BenefitTemplate extends Model
 {
     use HasFactory;
+
+    /**
+     * Campos que podem ser preenchidos em massa
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    /**
+     * Casts automáticos de datas
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public function benefits()
+    {
+        return $this->hasMany(Benefit::class);
+    }
 }
