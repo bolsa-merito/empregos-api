acc9a1f9f4f04eb188e487861872c2afe20b100f
THOMASW0LF <thomasgwolf1@gmail.com>
2025-07-01 16:47:40 -0300

    Atualiza model de Area com $fillable e $casts

--- app/Models/area.php
+++ app/Models/area.php
@@ -3,12 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class area extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name'];
+
+     protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
 
     public function courses()
     {
