ca693133fce5d96b5d78284fa4f0c6174239104e
THOMASW0LF <thomasgwolf1@gmail.com>
2025-07-01 16:50:23 -0300

    Atualiza model de Connection com $fillable e $casts

--- app/Models/Connection.php
+++ app/Models/Connection.php
@@ -3,10 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Connection extends Model
 {
     use HasFactory;
+
+     /**
+     * Campos preenchíveis em massa
+     */
+    protected $fillable = [
+        'student_id',
+        'company_id',
+        'status',
+    ];
+
+    /**
+     * Casts automáticos de datas
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
     
         public function student()
     {
