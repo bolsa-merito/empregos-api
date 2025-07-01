c095e29f356df73ce4be7d9d3c1c720a55da908e
THOMASW0LF <thomasgwolf1@gmail.com>
2025-07-01 16:46:57 -0300

    Atualiza model de Address com $fillable e $casts

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -3,10 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Address extends Model
 {
     use HasFactory;
+
+    /**
+     * Os campos que podem ser preenchidos via mass assignment
+     */
+    protected $fillable = [
+        'state',
+        'city',
+        'neighborhood',
+        'street',
+        'number',
+    ];
+
+    /**
+     * Converte automaticamente os timestamps para instâncias de Carbon
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
     
         public function companies()
     {
