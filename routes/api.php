4a9fe2ea212b37505327f77fda78db0b9a444aef
Hugo Batista <hugobatista2710@gmail.com>
2025-07-19 14:53:58 -0300

    feat: cria as rotas para studying

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CourseController;
 use App\Http\Controllers\Api\StudentController;
 use App\Http\Controllers\Api\InstitutionController;
+use App\Http\Controllers\Api\StudyingController;
 
 Route::apiResource('students', StudentController::class);
 
 Route::apiResource('courses', CourseController::class);
 
 Route::apiResource('institutions', InstitutionController::class);
+
+Route::apiResource('studying', StudyingController::class);
