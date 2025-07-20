35a695cf35848a272248307821b139cb6e0af3ed
Hugo Batista <hugobatista2710@gmail.com>
2025-07-20 12:04:48 -0300

    feat: cria a rota para certificates

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CourseController;
-use App\Http\Controllers\Api\ExperienceAndProjectController;
 use App\Http\Controllers\Api\StudentController;
-use App\Http\Controllers\Api\InstitutionController;
 use App\Http\Controllers\Api\StudyingController;
+use App\Http\Controllers\Api\CertificateController;
+use App\Http\Controllers\Api\InstitutionController;
+use App\Http\Controllers\Api\ExperienceAndProjectController;
 
 Route::apiResource('students', StudentController::class);
 
@@ -17,3 +18,5 @@ Route::apiResource('institutions', InstitutionController::class);
 Route::apiResource('studying', StudyingController::class);
 
 Route::apiResource('project-and-experience', ExperienceAndProjectController::class);
+
+Route::apiResource('certificates', CertificateController::class);
