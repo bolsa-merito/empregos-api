8a75c5c9375c8ab24c660a6c1757684105fb4fa0
Hugo Batista <hugobatista2710@gmail.com>
2025-07-20 15:07:42 -0300

    feat: cria as rotas para company e address

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CourseController;
+use App\Http\Controllers\Api\AddressController;
+use App\Http\Controllers\Api\CompanyController;
 use App\Http\Controllers\Api\StudentController;
 use App\Http\Controllers\Api\StudyingController;
 use App\Http\Controllers\Api\CertificateController;
@@ -20,3 +22,7 @@ Route::apiResource('studying', StudyingController::class);
 Route::apiResource('project-and-experience', ExperienceAndProjectController::class);
 
 Route::apiResource('certificates', CertificateController::class);
+
+Route::apiResource('company', CompanyController::class);
+
+Route::apiResource('address', AddressController::class);
