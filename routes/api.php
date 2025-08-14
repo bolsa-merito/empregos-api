70bc6cdca9b6f80f595ad40a09f8cc8c7e28f6a0
João Daniel M Weng <joaomweng@gmail.com>
2025-08-14 20:27:00 -0300

    Elementos base para a feature de feedback criadas (falta escrever as funções do CRUD na FeedbackController)

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CourseController;
+use App\Http\Controllers\Api\FeedbackController;
 use App\Http\Controllers\Api\StudentController;
 use App\Http\Controllers\Api\InstitutionController;
 
@@ -11,3 +12,5 @@ Route::apiResource('students', StudentController::class);
 Route::apiResource('courses', CourseController::class);
 
 Route::apiResource('institutions', InstitutionController::class);
+
+Route::apiResource('feedbacks', FeedbackController::class);
