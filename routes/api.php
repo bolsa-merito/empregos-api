ae5f11241fbd9698e2b1cad042c85939fff38ea2
Hugo Batista <hugobatista2710@gmail.com>
2025-08-19 15:14:41 -0300

    feat: add authenticated routes for managing connections

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AreaController;
+use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Api\CourseController;
 use App\Http\Controllers\Api\AddressController;
-use App\Http\Controllers\Api\AreaController;
 use App\Http\Controllers\Api\CompanyController;
 use App\Http\Controllers\Api\StudentController;
 use App\Http\Controllers\Api\StudyingController;
 use App\Http\Controllers\Api\CertificateController;
+use App\Http\Controllers\Api\ConnectionsController;
 use App\Http\Controllers\Api\InstitutionController;
-use App\Http\Controllers\Api\ExperienceAndProjectController;
-use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Auth\EmailVerificationController;
+use App\Http\Controllers\Api\ExperienceAndProjectController;
 
 Route::apiResource('students', StudentController::class)->except(['update']);
 
@@ -26,7 +27,14 @@ Route::apiResource('project-and-experience', ExperienceAndProjectController::cla
 
 Route::apiResource('certificates', CertificateController::class);
 
-Route::apiResource('company', CompanyController::class);
+// rota pública
+Route::get('/companies', [CompanyController::class, 'index']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/company', [CompanyController::class, 'show']);
+    Route::post('/company', [CompanyController::class, 'store']);
+    Route::put('/company', [CompanyController::class, 'update']);
+    Route::delete('/company', [CompanyController::class, 'destroy']);
+});
 
 Route::apiResource('address', AddressController::class);
 
@@ -44,7 +52,18 @@ Route::prefix('students/update')->group(function () {
         Route::post('/certificate', [CertificateController::class, 'storeAuthenticated']);
         Route::put('/certificate/{certificate}', [CertificateController::class, 'updateAuthenticated']);
         Route::delete('/certificate/{certificate}', [CertificateController::class, 'destroyAuthenticated']);
+
+        Route::post('/studying', [StudyingController::class, 'storeAuthenticated']);
+        Route::put('/studying/{studying}', [StudyingController::class, 'updateAuthenticated']);
+        Route::delete('/studying/{studying}', [StudyingController::class, 'destroyAuthenticated']);
     });
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/connections', [ConnectionsController::class, 'index']);
+    Route::post('/connections', [ConnectionsController::class, 'store']);
+    Route::put('/connections/{connection}', [ConnectionsController::class, 'update']);
+    Route::delete('/connections/{connection}', [ConnectionsController::class, 'destroy']);
 });
 
 // Rotas de autenticação
